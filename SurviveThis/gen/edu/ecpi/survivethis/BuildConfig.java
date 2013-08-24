549eacd2c92df8fda53000d75b6d9bb02f988817
xyclos <jake@xyclos.com>
2013-08-24 01:28:57 -0400

    Initial Commit
    Backup so we don't lose everything.

---
+++ SurviveThis/gen/edu/ecpi/survivethis/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.ecpi.survivethis;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SurviveThis/src/edu/ecpi/survivethis/MainActivity.java
@@ -0,0 +1,67 @@
+package edu.ecpi.survivethis;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button woodsBt = (Button) findViewById(R.id.woods);
+        woodsBt.setOnClickListener(onClickListener);
+        Button desertBt = (Button) findViewById(R.id.desert);
+        desertBt.setOnClickListener(onClickListener);
+        Button snowBt = (Button) findViewById(R.id.snow);
+        snowBt.setOnClickListener(onClickListener);
+        Button waterBt = (Button) findViewById(R.id.water);
+        waterBt.setOnClickListener(onClickListener);
+        Button swampBt = (Button) findViewById(R.id.swamp);
+        swampBt.setOnClickListener(onClickListener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    private OnClickListener onClickListener = new OnClickListener() {
+    	public void onClick(View v) {
+    		switch(v.getId()) {
+	    		case R.id.woods:
+	    			Intent woodsIntent = new Intent(MainActivity.this, WoodsActivity.class);
+	    			startActivity(woodsIntent);
+	    		break;
+	    		case R.id.desert:
+	    			Intent desertIntent = new Intent(MainActivity.this, DesertActivity.class);
+	    			startActivity(desertIntent);
+	    		break;
+	    		case R.id.snow:
+	    			Intent snowIntent = new Intent(MainActivity.this, SnowActivity.class);
+	    			startActivity(snowIntent);
+	    		break;
+	    		case R.id.water:
+	    			Intent waterIntent = new Intent(MainActivity.this, WaterActivity.class);
+	    			startActivity(waterIntent);
+	    		break;
+	    		case R.id.swamp:
+	    			Intent swampIntent = new Intent(MainActivity.this, SwampActivity.class);
+	    			startActivity(swampIntent);
+	    		break;
+    		}
+        	
+        }
+    };
+    
+    
+}
